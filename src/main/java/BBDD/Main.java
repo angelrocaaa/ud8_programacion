2eb153ccee9dc1def264012fdac67e70561e7b2d
A720\dam1 <rocamora29angel@gmail.com>
2026-04-17 19:15:44 +0200

    Conexión base de datos

--- src/main/java/BBDD/Main.java
+++ src/main/java/BBDD/Main.java
@@ -1,13 +1,17 @@
 package BBDD;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
-
         public static void main(String[] args) {
+
             Connection bd = conexion();
+            System.out.println("Realizando consultas...");
+            consulta(bd);
+            modificar(bd);
+            consulta(bd);
+            desconectar(bd);
+
         }
 
         public static Connection conexion() {
@@ -15,7 +19,7 @@ public class Main {
             String host = "jdbc:mariadb://localhost:3310/";
             String user = "root";
             String psw = "";
-            String bd = "instituto";
+            String bd = "colegio";
             System.out.println("Conectando...");
 
             try {
@@ -29,4 +33,54 @@ public class Main {
             return conexion;
         }
 
+        public static void desconectar(Connection conexion){
+
+            System.out.println("Desconectando...");
+
+            try {
+                conexion.close();
+                System.out.println("Conexión finalizada.");
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+                throw new RuntimeException(e);
+            }
+        }
+
+        public static void consulta(Connection connection) {
+            String query = "SELECT * FROM estudiante";
+
+            Statement statement;
+
+            try {
+                statement = connection.createStatement();
+                ResultSet respuesta = statement.executeQuery(query);
+
+                while (respuesta.next()) {
+                    int nia = respuesta.getInt("nia");
+                    String nombre = respuesta.getString("nombre");
+                    Date fecha_nacimiento = respuesta.getDate("fecha_nacimiento");
+
+                    System.out.println("Estudiante con NIA: " + nia + " nombre " + nombre + " fecha de nacimiento " + fecha_nacimiento);
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+                throw new RuntimeException(e);
+            }
+        }
+
+        public static void modificar(Connection connection) {
+            String query = "UPDATE estudiante SET nombre='angel' WHERE nia='23434'";
+
+            Statement statement;
+
+            try {
+                statement = conexion().createStatement();
+                statement.executeQuery(query);
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+                throw new RuntimeException(e);
+            }
+        }
+
 }
+
